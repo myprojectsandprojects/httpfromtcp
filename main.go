bc3ad23659f6c77c6625e2cad6a750947131c8ad
Eero <eerolillemae@gmail.com>
2025-11-07 20:08:40 +0200

    reading from a file

--- main.go
+++ main.go
@@ -2,8 +2,26 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"log"
+	"io"
 )
 
 func main() {
-	fmt.Println("Hello, TCP and HTTP!")
+	f, err := os.Open("message.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	b := make([]byte, 8)
+	for {
+		n, err := f.Read(b)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			log.Fatal(err)
+		}
+		fmt.Printf("read %v bytes: %s\n", n, b[:n])
+	}
 }
