247d9a88af53c8be61de67642648432174a5ff1b
Eero <eerolillemae@gmail.com>
2026-01-24 11:13:46 +0200

    HTML

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -33,16 +33,46 @@ func handler(res *response.Response, req *request.Request) {
 	case "/yourproblem":
 		res.SetStatusCode(response.StatusCode_400)
 		res.SetDefaultHeaders()
-		res.Headers.Set("Content-Type", "text/plain")
-		res.Body("Your problem is not my problem\n")
+		res.Headers.Set("Content-Type", "text/html")
+		body :=
+			`<html>
+  <head>
+    <title>400 Bad Request</title>
+  </head>
+  <body>
+    <h1>Bad Request</h1>
+    <p>Your request honestly kinda sucked.</p>
+  </body>
+</html>`
+		res.Body(body)
 	case "/myproblem":
 		res.SetStatusCode(response.StatusCode_500)
 		res.SetDefaultHeaders()
-		res.Headers.Set("Content-Type", "text/plain")
-		res.Body("Whoopsie, my bad\n")
+		res.Headers.Set("Content-Type", "text/html")
+		body :=
+			`<html>
+  <head>
+    <title>500 Internal Server Error</title>
+  </head>
+  <body>
+    <h1>Internal Server Error</h1>
+    <p>Okay, you know what? This one is on me.</p>
+  </body>
+</html>`
+		res.Body(body)
 	default:
 		res.SetDefaultHeaders()
-		res.Headers.Set("Content-Type", "text/plain")
-		res.Body("All good frfr\n")
+		res.Headers.Set("Content-Type", "text/html")
+		body :=
+			`<html>
+  <head>
+    <title>200 OK</title>
+  </head>
+  <body>
+    <h1>Success!</h1>
+    <p>Your request was an absolute banger.</p>
+  </body>
+</html>`
+		res.Body(body)
 	}
 }
